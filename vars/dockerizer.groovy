37aa12439b903cc2dc08ecae643f2f9442f364b6
StasX <s.mestechkin@gmail.com>
2026-07-30 20:16:42 +0300

    Add library files

---
+++ vars/dockerizer.groovy
@@ -0,0 +1,51 @@
+def build(String image, String version="latest", String envName=""){
+    echo "Building docker image..."
+    if(envName==""){
+        sh "docker build -t docker.io/${image}:${version} ."
+        return 0
+    }
+    sh "docker build -t docker.io/${repoOwner}/${image}:${version}-${envName} ."
+}
+
+def tag(String image, String version="latest", String envName=""){
+    if (version=="latest"){
+        return 0
+    }
+    echo "Tagging docker image..."
+    if(envName==""){
+        sh """
+        docker tag \
+        docker.io/${image}:${version} \
+        docker.io/${image}:latest
+        """
+        return 0
+    }
+    sh """
+    docker tag \
+    docker.io/${image}:${version}-${envName} \
+    docker.io/${image}:latest-${envName}
+    """
+}
+
+def login(){
+    echo "Logging in to Docker registry..."
+    withCredentials([usernamePassword(credentialsId: "dockerhub-creds", usernameVariable: "DOCKERHUB_USERNAME", passwordVariable: "DOCKERHUB_PASSWORD")]) {
+        sh "docker login -u ${DOCKERHUB_USERNAME} -p ${DOCKERHUB_PASSWORD} docker.io"
+    }
+}
+
+def push(String image, String version="latest", String envName=""){
+    echo "Pushing docker image to registry..."
+    if(envName==""){
+        if (version != "latest"){
+            sh "docker push docker.io/${image}:${version}"
+        }
+        sh "docker push  docker.io/${image}:latest"
+        return  0
+    }
+    sh """
+    docker push docker.io/${image}:${version}-${envShortName}
+    docker push  docker.io/${image}:latest-${envShortName}
+    """
+}
+              
