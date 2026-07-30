d953dfefedd7b8f2f99cfc367268c5712bf50fb6
StasX <s.mestechkin@gmail.com>
2026-07-31 00:34:30 +0300

    Fix violations

--- vars/dockerizer.groovy
+++ vars/dockerizer.groovy
@@ -2,24 +2,28 @@ def buildImage(
     String image,
     String version = 'latest',
     String envName = ''
-){
+) {
     echo 'Building docker image...'
+
     if (!envName) {
         sh "docker build -t docker.io/${image}:${version} ."
         return 0
     }
+
     sh "docker build -t docker.io/${image}:${version}-${envName} ."
 }
 
 def tag(
     String image,
-    String version = "latest",
+    String version = 'latest',
     String envName = ''
-){
+) {
     if (version == 'latest') {
         return 0
     }
+
     echo 'Tagging docker image...'
+
     if (!envName) {
         sh """
         docker tag \
@@ -28,6 +32,7 @@ def tag(
         """
         return 0
     }
+
     sh """
     docker tag \
     docker.io/${image}:${version}-${envName} \
@@ -35,19 +40,20 @@ def tag(
     """
 }
 
-def login(){
+def login() {
     echo 'Logging in to Docker registry...'
+
     withCredentials([
         usernamePassword(
-            credentialsId: 'dockerhub-creds',
-            usernameVariable: 'DOCKERHUB_USERNAME',
-            passwordVariable: 'DOCKERHUB_PASSWORD'
+            credentialsId:'dockerhub-creds',
+            usernameVariable:'DOCKERHUB_USERNAME',
+            passwordVariable:'DOCKERHUB_PASSWORD'
         )
-    ]){
+    ]) {
         withEnv([
             "DOCKER_USERNAME=${DOCKERHUB_USERNAME}",
             "DOCKER_PASSWORD=${DOCKERHUB_PASSWORD}"
-        ]){
+        ]) {
             sh '''
             echo "$DOCKER_PASSWORD" | \
             docker login \
@@ -62,17 +68,20 @@ def push(
     String image,
     String version = 'latest',
     String envName = ''
-){
+) {
     echo 'Pushing docker image to registry...'
+
     if (!envName) {
         if (version != 'latest') {
             sh "docker push docker.io/${image}:${version}"
         }
-        sh "docker push  docker.io/${image}:latest"
-        return  0
+
+        sh "docker push docker.io/${image}:latest"
+        return 0
     }
+
     sh """
     docker push docker.io/${image}:${version}-${envName}
-    docker push  docker.io/${image}:latest-${envName}
+    docker push docker.io/${image}:latest-${envName}
     """
 }
